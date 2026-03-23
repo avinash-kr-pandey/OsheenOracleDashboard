6bdb9792bf4983a83e865a2cac088f708570add8
Avinash <avinash@goldceownlabs.com>
2026-03-23 13:02:15 +0530

    new code

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,8 @@
 // utils/api.ts
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-// const API_BASE_URL = "https://osheenoraclebackend02-4oz7.onrender.com/api";
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://osheenoraclebackend02-4oz7.onrender.com/api";
+// const API_BASE_URL = "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
